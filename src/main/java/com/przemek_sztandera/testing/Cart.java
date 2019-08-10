cb4e41b55e3e6efaaf4412b10fdb1b20cc02f668
Przemek <38491818+PrzemekSztandera@users.noreply.github.com>
2019-08-10 21:50:25 +0100

    Timeout.

---
+++ src/main/java/com/przemek_sztandera/testing/Cart.java
@@ -0,0 +1,29 @@
+package com.przemek_sztandera.testing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+
+    private List<Order> orders = new ArrayList<>();
+
+    void addOrderToCart(Order order) {
+        this.orders.add(order);
+    }
+
+    void clearCart() {
+        this.orders.clear();
+    }
+
+    void simulateLargeOrder() {
+        for(int i= 0; i < 1_000; i++) {
+            Meal meal = new Meal(10, "hamburger");
+            Order order = new Order();
+            order.addMealToOrder(meal);
+            addOrderToCart(order);
+        }
+        System.out.println("Cart size: " + orders.size());
+        clearCart();
+
+    }
+}
---
+++ src/test/java/com/przemek_sztandera/testing/CartTest.java
@@ -0,0 +1,20 @@
+package com.przemek_sztandera.testing;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartTest {
+
+    @Test
+    void simulateLargeOrder() {
+        // given
+        Cart cart = new Cart();
+
+        // when
+        // then
+        assertTimeout(Duration.ofMillis(100), cart::simulateLargeOrder);
+    }
+}
